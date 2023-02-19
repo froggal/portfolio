73a168d6900220afa44f366f096bab81fc03746b
froggal <doeon111@naver.com>
2023-02-19 19:20:24 +0900

    components setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import Layout from 'components/layout'
 export default function Home() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>키페의 포트폴리오</title>
-        <meta name="description" content="D.E. Kim's Portfolio" />
+        <meta name="description" content="키페의 포트폴리오" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>키페(KeyFrog)</h1>
-    </>
+      <h1>홈</h1>
+    </Layout>
   )
 }
