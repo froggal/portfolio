c66a4d4bacaaa85b31afefbe43e0c1a82159b285
froggal <doeon111@naver.com>
2023-05-29 16:55:15 +0900

    create a scroll animation.

---
+++ script.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", revealText);
+
+function revealText() {
+  var header = document.querySelector(".header");
+  var position = header.getBoundingClientRect().top;
+  var windowHeight = window.innerHeight;
+
+  if (position < windowHeight / 1.5) {
+    header.classList.add("header-visible");
+  } else {
+    header.classList.remove("header-visible");
+  }
+}
